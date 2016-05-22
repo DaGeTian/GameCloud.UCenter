fedb873a04d7085b5a15e2bf69d3211fafb8eadd
yan <nbcloudstudio@hotmail.com>
2016-05-22 13:08:03 +0800

    Add missing files

--- GF.UCenter.CouchBase/Database/CouchBucketExtensions.cs
+++ GF.UCenter.CouchBase/Database/CouchBucketExtensions.cs
@@ -7,7 +7,7 @@
     using System.Threading.Tasks;
     using Couchbase.Core;
     using Couchbase.N1QL;
-
+    using Models;
     public static class CouchBucketExtensions
     {
         public static async Task<IQueryResult<TEntity>> QueryAsync<TEntity>(this IBucket bucket,
@@ -140,6 +140,32 @@
             var result = await bucket.QuerySlimAsync(expression, throwIfFailed);
 
             return result.FirstOrDefault();
+        }
+
+        public static async Task<PaginationResponse<TEntity>> PaginationQuerySlimAsync<TEntity>(
+            this IBucket bucket, PaginationQueryExpression<TEntity> expression) where TEntity : class, IBaseEntity
+        {
+            var countRequest = expression.BuildQueryCountRequest(bucket);
+            var countResult = await bucket.QueryAsync<CountRaw>(countRequest);
+            if (!countResult.Success)
+            {
+                throw countResult.Exception;
+            }
+
+            var rawRequest = expression.BuildQueryRawRequest(bucket);
+            var rawResult = await bucket.QueryAsync<TEntity>(rawRequest);
+            if (!rawResult.Success)
+            {
+                throw rawResult.Exception;
+            }
+
+            PaginationResponse<TEntity> response = new PaginationResponse<TEntity>();
+            response.Page = expression.Page;
+            response.PageSize = expression.PageSize;
+            response.Total = countResult.Rows.First().Count;
+            response.Raws = rawResult.Rows;
+
+            return response;
         }
     }
 }
---
+++ GF.UCenter.CouchBase/Models/PaginationResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GF.UCenter.CouchBase.Models
+{
+    public class PaginationResponse<TRaw>
+    {
+        public int Total { get; set; }
+
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+        
+        public IEnumerable<TRaw> Raws { get; set; }
+    }
+}
