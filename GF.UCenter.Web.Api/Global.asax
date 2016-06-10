8cb9c765d3e84e30ad9193f352730b0aa854b179
yan <nbcloudstudio@hotmail.com>
2016-06-10 15:02:29 +0800

    Clean up used code in web api project

--- GF.UCenter.Web.Api/Global.asax
+++ GF.UCenter.Web.Api/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GF.UCenter.Web.Api.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GF.UCenter.Web.Api.WebApiApplication" Language="C#" %>
--- GF.UCenter.Web.Api/Global.asax.cs
+++ GF.UCenter.Web.Api/Global.asax.cs
@@ -3,8 +3,6 @@
     using System.ComponentModel.Composition.Hosting;
     using System.Web;
     using System.Web.Http;
-    using System.Web.Mvc;
-    using System.Web.Optimization;
     using System.Web.Routing;
     using Common;
     using UCenter.Common;
@@ -13,21 +11,17 @@
     /// <summary>
     /// MVC Application
     /// </summary>
-    public class MvcApplication : HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         /// <summary>
         /// The application start event
         /// </summary>
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // MEF initiliazation
             ExportProvider exportProvider = CompositionContainerFactory.Create();
-
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-
             WebApplicationManager.InitializeApplication(GlobalConfiguration.Configuration, exportProvider);
             SettingsInitializer.Initialize<Settings>(
                 exportProvider,
