881c87cb2207813c5c4aa027054fbca25cd7f678
yan <nbcloudstudio@hotmail.com>
2016-07-17 22:45:36 +0800

    Manager.TexasPoker static pages

--- GF.Manager.TexasPoker/Controllers/HomeController.cs
+++ GF.Manager.TexasPoker/Controllers/HomeController.cs
@@ -12,5 +12,48 @@ namespace GF.Manager.TexasPoker.Controllers
         {
             return View();
         }
+
+        public ActionResult UserList()
+        {
+            return View();
+        }
+
+        public ActionResult RoboList()
+        {
+            return View();
+        }
+
+        public ActionResult BuyChipsEvents() { return View(); }
+        public ActionResult ChipBuyGiftEvents() { return View(); }
+        public ActionResult ChipBuyItemEvents() { return View(); }
+        public ActionResult DailyGetChipsEvents() { return View(); }
+        public ActionResult LostAllSendChipsEvents() { return View(); }
+        public ActionResult PlayerSendOtherChipsEvents() { return View(); }
+        public ActionResult PlayerGetChipsFromOtherEvents() { return View(); }
+        public ActionResult BuyCoinsEvents() { return View(); }
+        public ActionResult CoinBuyGiftEvents() { return View(); }
+        public ActionResult CoinBuyItemEvents() { return View(); }
+        public ActionResult BuyVIPEvents() { return View(); }
+        public ActionResult CardGameEvents() { return View(); }
+
+        public ActionResult EventReport()
+        {
+            return View();
+        }
+
+        public ActionResult Analytics()
+        {
+            return View();
+        }
+
+        public ActionResult AppConfig()
+        {
+            return View();
+        }
+
+        public ActionResult ClubList()
+        {
+            return View();
+        }
     }
 }
