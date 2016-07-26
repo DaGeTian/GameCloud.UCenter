2c31ec501bb9714bba234f9c44fbde454de3ac7b
nbcloudstudio <nbcloudstudio@hotmail.com>
2016-07-26 22:43:57 +0800

    Add icon to menus

--- GF.Manager.UCenter/App_Start/BundleConfig.cs
+++ GF.Manager.UCenter/App_Start/BundleConfig.cs
@@ -11,9 +11,6 @@ namespace GF.Manager.UCenter
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/metisMenu.js",
@@ -25,6 +22,7 @@ namespace GF.Manager.UCenter
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/metisMenu.css",
                       "~/Content/sb-admin-2.css",
                       "~/Content/angular-chart.css",
