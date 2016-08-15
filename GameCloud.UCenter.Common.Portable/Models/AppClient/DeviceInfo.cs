782e71a0e25fec7a52848e0a68073395f58dbc07
yan <nbcloudstudio@hotmail.com>
2016-08-15 09:58:20 +0800

    Fix u3d sdk build break

--- GameCloud.UCenter.Common.Portable/Models/AppClient/DeviceInfo.cs
+++ GameCloud.UCenter.Common.Portable/Models/AppClient/DeviceInfo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GameCloud.UCenter.Common.Portable.Models.AppClient
+﻿namespace GameCloud.UCenter.Common.Portable.Models.AppClient
 {
     public class DeviceInfo
     {
