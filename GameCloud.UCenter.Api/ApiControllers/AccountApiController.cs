96fe3685262c7d3418d2c454955856ec860ce9cb
nbcloudstudio <nbcloudstudio@hotmail.com>
2016-09-09 00:32:07 +0800

    Rollback ip change

--- GameCloud.UCenter.Api/ApiControllers/AccountApiController.cs
+++ GameCloud.UCenter.Api/ApiControllers/AccountApiController.cs
@@ -408,7 +408,7 @@ namespace GameCloud.UCenter.Api.ApiControllers
             string message = null,
             CancellationToken token = default(CancellationToken))
         {
-            var clientIp = Request.HttpContext.Connection.RemoteIpAddress;// IPHelper.GetClientIpAddress(Request);
+            var clientIp = "";// IPHelper.GetClientIpAddress(Request);
 
             var errorEvent = new ErrorEventEntity()
             {
