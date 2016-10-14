9efee390fb4043dafb4a3b48147dd2fcdcf45e2a
nbcloudstudio <nbcloudstudio@hotmail.com>
2016-10-14 19:24:02 +0800

    Add wechat auth api

---
+++ GameCloud.UCenter.Api/ApiControllers/MockApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameCloud.UCenter.Common.Portable.Models.AppClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameCloud.UCenter.Api.ApiControllers
+{
+    public class MockApiController : Controller
+    {
+        /// <summary>
+        /// Mock https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code
+        /// Doc: https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1419317853&token=&lang=zh_CN
+        /// </summary>
+        /// <param name="appid"></param>
+        /// <param name="secret"></param>
+        /// <param name="code"></param>
+        /// <param name="grant_type"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/mock/sns/oauth2/access_token")]
+        public WeChatAccessTokenResponse GetAuthTokenResponse(string appid, string secret, string code, string grant_type)
+        {
+            string scope = code == "codeforsnsapi_userinfo" ? "snsapi_userinfo" : "snsapi_base";
+            var result = new WeChatAccessTokenResponse
+            {
+                OpenId = "mock_openid",
+                AccessToken = "mock_access_token",
+                ExpiresIn = 7200,
+                RefreshToken = "mock_refresh_token",
+                Scope = scope,
+                UnionId = "mock_unionid"
+            };
+
+            return result;
+        }
+    }
+}
