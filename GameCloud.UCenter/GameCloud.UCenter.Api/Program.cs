830f4a104903c4e5a9f9384d54080747fca5b474
lion.lee <lion@cragon.cn>
2016-10-24 17:29:08 +0800

    UseUrls改为从配置文件hosting.json中读取。

--- GameCloud.UCenter/GameCloud.UCenter.Api/Program.cs
+++ GameCloud.UCenter/GameCloud.UCenter.Api/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace GameCloud.UCenter.Api
 {
@@ -7,9 +11,15 @@ namespace GameCloud.UCenter.Api
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
